78130dc8125180c2f425c685f1596b235b7b4034
sailendrachettri <sailendra9083@gmail.com>
2023-12-24 22:17:24 +0530

    user able to register - now go for pwd encryption

--- blog/src/pages/Register.js
+++ blog/src/pages/Register.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
+
 export default function Register(){
+    // STATE VARIABLES
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // FUNCTIONS
+    async function register(ev){
+        ev.preventDefault();
+
+        const response = await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-type': 'application/json'}
+        })
+
+        if(response.status === 200)
+            alert("User created successfully!");
+        else
+            alert('Registration failed')
+    }
+
     return(
         <>
-            <form className="register">
+            <form className="register" onSubmit={register}>
                 <h1>Register</h1>
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
+                <input type="text" 
+                    placeholder="username" 
+                    value={username}
+                    onChange={ev => setUsername(ev.target.value)} 
+                />
+                <input type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={ev => setPassword(ev.target.value)}
+                />
                 <button>Register</button>
             </form>
         </>
